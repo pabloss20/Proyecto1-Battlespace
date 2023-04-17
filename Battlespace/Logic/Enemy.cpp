6da1f229805610db67dc5adb8952256cdbaa36fd
Pablo Sánchez <pablosanchez19@estudiantec.cr>
2023-04-17 08:17:47 -0600

    Desarrollo de lógica implementado

--- Battlespace/Logic/Enemy.cpp
+++ Battlespace/Logic/Enemy.cpp
@@ -13,3 +13,76 @@ Enemy::Enemy(Texture *texture, int id, int x_pos, int y_pos, int health, int mov
 
     this->sprite.setTexture(*this->texture);
 }
+
+
+void Enemy::draw(RenderTarget &target)
+{
+    this->sprite.setPosition(this->x_pos, this->y_pos);
+    target.draw(this->sprite);
+}
+
+void Enemy::update(Vector2u window_bound){}
+
+void Enemy::move(int idm)
+{
+    switch (idm)
+    {
+        case 0:
+
+            while (this->getXpos() > -10)
+            {
+                this->sprite.move(-10, 0);
+                this->setXpos(this->getXpos() - 10);
+            }
+            break;
+
+        case 1:
+
+            while (this->getXpos() > -10)
+            {
+                while (this->getYpos() > 0)
+                {
+                    if (this->getYpos() == 0)
+                    {
+                        while (this->getYpos() < 901)
+                        {
+                            this->sprite.move(-10, 10);
+                            this->setYpos(this->getYpos() + 10);
+                        }
+                    }
+                    this->sprite.move(-10, -10);
+                    this->setYpos(this->getYpos() - 10);
+                }
+                this->setXpos(this->getXpos() - 10);
+            }
+            break;
+    }
+}
+
+void Enemy::setXpos(int pos)
+{
+    this->x_pos = pos;
+}
+
+int Enemy::getXpos()
+{
+    return this->x_pos;
+}
+
+void Enemy::setYpos(int pos)
+{
+    this->y_pos = pos;
+}
+
+int Enemy::getYpos()
+{
+    return this->y_pos;
+}
+
+void Enemy::setHealth(int healthx) {
+    this->health = healthx;
+}
+
+int Enemy::getHealth() {
+    return this->health;
+}
