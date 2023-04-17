fcc68eda2c74d2a0a2dec8d2772c14740e6eea9a
Pablo Sánchez <pablosanchez19@estudiantec.cr>
2023-04-17 08:14:25 -0600

    Creación de la clase Enemy como Nodo

---
+++ Battlespace/Headers/Enemy.h
@@ -0,0 +1,49 @@
+
+#ifndef BATTLESPACE_ENEMY_H
+#define BATTLESPACE_ENEMY_H
+
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+using namespace sf;
+
+class Enemy
+{
+public:
+
+    // Constructor
+    Enemy(){id = 0; next = NULL;}
+
+    // Parametized constructor
+    Enemy(Texture *texture, int id, int x_pos, int y_pos, int health, int movement);
+
+    int id;
+    int x_pos;
+    int y_pos;
+    int health;
+    int movement;
+
+    Enemy *next;
+
+    void draw(RenderTarget &target);
+    void update(Vector2u window_bounds);
+    void move(int);
+    void setXpos(int);
+    int getXpos();
+    void setYpos(int);
+    int getYpos();
+    void setHealth(int);
+    int getHealth();
+
+
+private:
+
+    Texture *texture;
+    RectangleShape hit_box;
+    Sprite sprite;
+
+};
+
+#endif //BATTLESPACE_ENEMY_H
